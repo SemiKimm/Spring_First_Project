46e8c7739b58575699ca908205c757596d945fe5
김세미 <semikimm.m@gmail.com>
2022-04-14 05:57:11 +0900

    [spring_day3_hw] 단계 3 : Springframework Logging 사용위한 설정 - spring-jcl & logback 의존성 추가

---
+++ springframework-project1-kimsemi/src/main/java/com/nhnacademy/edu/springframework/project/aspect/LoggingAspect.java
@@ -0,0 +1,10 @@
+package com.nhnacademy.edu.springframework.project.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+}
