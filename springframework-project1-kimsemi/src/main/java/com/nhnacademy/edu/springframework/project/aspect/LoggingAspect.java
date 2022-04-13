fcb6844c229f90fe250cb0623099d73d39ceb1de
김세미 <semikimm.m@gmail.com>
2022-04-14 06:33:48 +0900

    [spring_day3_hw] 단계 3 : 실행시간 측정 & log 파일에 메소드 명과 수행 시간 기록 완료

--- springframework-project1-kimsemi/src/main/java/com/nhnacademy/edu/springframework/project/aspect/LoggingAspect.java
+++ springframework-project1-kimsemi/src/main/java/com/nhnacademy/edu/springframework/project/aspect/LoggingAspect.java
@@ -1,10 +1,28 @@
 package com.nhnacademy.edu.springframework.project.aspect;
 
+import java.util.logging.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
 public class LoggingAspect {
-
+    private static final Log log = LogFactory.getLog(LoggingAspect.class);
+    @Around("within(com.nhnacademy.edu.springframework.project.service..*)")
+    public Object logSendMessage(ProceedingJoinPoint ptj) throws Throwable{
+        StopWatch stopWatch = new StopWatch();
+        try{
+            stopWatch.start();
+            return ptj.proceed();
+        }finally {
+            stopWatch.stop();
+            System.out.println(ptj.getSignature()+":"+stopWatch.prettyPrint());
+            log.info(ptj.getSignature()+":"+stopWatch.prettyPrint());
+        }
+    }
 }
