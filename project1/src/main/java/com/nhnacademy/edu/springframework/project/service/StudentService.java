8be412bd76ba82394798a47aa876cbd8dd7ccc44
김세미 <semikimm.m@gmail.com>
2022-04-13 15:19:08 +0900

    [spring_day3_hw] 디렉토리명 변경

--- project1/src/main/java/com/nhnacademy/edu/springframework/project/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.edu.springframework.project.service;
-
-import java.util.Collection;
-
-public interface StudentService {
-    Collection<Student> getPassedStudents();
-
-    Collection<Student> getStudentsOrderByScore();
-}
