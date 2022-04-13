c7d11f0d932c74a1476bb97d3be03a4f15e61ebb
김세미 <semikimm.m@gmail.com>
2022-04-14 00:28:30 +0900

    [spring_day3_hw] ScoresTest 수정 완료

--- springframework-project1-kimsemi/src/test/java/com/nhnacademy/edu/springframework/project/repository/ScoresTest.java
+++ springframework-project1-kimsemi/src/test/java/com/nhnacademy/edu/springframework/project/repository/ScoresTest.java
@@ -1,12 +1,11 @@
 package com.nhnacademy.edu.springframework.project.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,13 +20,30 @@ class ScoresTest {
         csvScores = CsvScores.getInstance();
     }
 
-    @DisplayName("load() 한 후에 findAll()을 통해 불러온 List<Score> result는 비어있으면 안된다.")
     @Test
     void load() {
+        assertDoesNotThrow(() -> csvScores.load());
+    }
+
+    @DisplayName("load() 한 후에 findAll()을 통해 불러온 List<Score> result는 csv가 비어있지 않는한 비어있지 않아야 한다.")
+    @Test
+    void load_resultIsNotEmpty() {
+        int size = 0;
+        try (BufferedReader scoreFileReader = new BufferedReader
+            (new InputStreamReader(Objects.requireNonNull(
+                getClass().getClassLoader().getResourceAsStream("data/score.csv"))))) {
+            while(scoreFileReader.readLine()!=null){
+                size++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         csvScores.load();
         List<Score> result = csvScores.findAll();
-        assertThat(result)
-            .isNotEmpty();
+        if(size!=0){
+            assertThat(result)
+                .isNotEmpty();
+        }
     }
 
     @Test
@@ -36,8 +52,7 @@ class ScoresTest {
         try (BufferedReader scoreFileReader = new BufferedReader
             (new InputStreamReader(Objects.requireNonNull(
                 getClass().getClassLoader().getResourceAsStream("data/score.csv"))))) {
-            String l;
-            while ((l = scoreFileReader.readLine()) != null) {
+            while (scoreFileReader.readLine() != null) {
                 size++;
             }
         } catch (IOException e) {
