aef09aab832dbbafc850a307d0b7f6ee1d2518bb
김세미 <semikimm.m@gmail.com>
2022-04-14 01:35:34 +0900

    [spring_day3_hw] StudentServiceTest 완료 (Student student 이렇게 만든 student에 왜 foreach 안에서 재할당이 안되는거지..?)

--- springframework-project1-kimsemi/src/test/java/com/nhnacademy/edu/springframework/project/service/StudentServiceTest.java
+++ springframework-project1-kimsemi/src/test/java/com/nhnacademy/edu/springframework/project/service/StudentServiceTest.java
@@ -1,16 +1,49 @@
 package com.nhnacademy.edu.springframework.project.service;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.nhnacademy.edu.springframework.project.repository.CsvScores;
+import com.nhnacademy.edu.springframework.project.repository.CsvStudents;
+import com.nhnacademy.edu.springframework.project.repository.Score;
+import com.nhnacademy.edu.springframework.project.repository.Scores;
+import com.nhnacademy.edu.springframework.project.repository.Students;
+import java.util.Collection;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.csv.Csv;
 
 class StudentServiceTest {
+    StudentService defaultStudentService;
+
+    @BeforeEach
+    void setUp() {
+        defaultStudentService = new DefaultStudentService();
+    }
 
     @Test
     void getPassedStudents() {
+        DataLoadService dataLoadService = new CsvDataLoadService();
+        dataLoadService.loadAndMerge();
+
+        Score passedScore = new Score(0, 60);
+        Collection<Student> result = defaultStudentService.getPassedStudents();
+        for (Student student : result) {
+            assertThat(student.getScore().getScore()>=passedScore.getScore()).isTrue();
+        }
+
     }
 
     @Test
     void getStudentsOrderByScore() {
+        DataLoadService dataLoadService = new CsvDataLoadService();
+        dataLoadService.loadAndMerge();
+
+        Collection<Student> result = defaultStudentService.getStudentsOrderByScore();
+        Student[] prevStudent = {result.stream().findFirst().get()};
+        result.forEach(student -> {
+            assertThat(prevStudent[0].getScore().getScore()>=student.getScore().getScore()).isTrue();
+            prevStudent[0] = student;
+        });
     }
 }
